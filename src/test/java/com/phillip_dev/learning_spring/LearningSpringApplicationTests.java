f3b676851261735a8deb64f212177c10a90d05b0
PHILLIP26 <phillip26@discovery.co.za>
2024-01-24 17:36:11 +0200

    proj init

---
+++ src/test/java/com/phillip_dev/learning_spring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.learning_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
