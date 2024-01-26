44af36f12d4fffc7be1d356038bb08186a479022
PHILLIP26 <phillip26@discovery.co.za>
2024-01-26 22:31:33 +0200

    add controller

--- src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
+++ src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
@@ -1,8 +1,12 @@
 package com.phillip_dev.markssubmittionproj;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MarkController {
-    
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Somehtmlview";
+    }
 }
