1dea0fb9912986f8461a538bac11c9f7e755d94f
PHILLIP26 <phillip26@discovery.co.za>
2024-01-26 23:04:42 +0200

    proj init

---
+++ src/main/java/com/phillip_dev/handlegetrequest/HandlegetrequestApplication.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.handlegetrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlegetrequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlegetrequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phillip_dev/handlegetrequest/HandlegetrequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.handlegetrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlegetrequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
