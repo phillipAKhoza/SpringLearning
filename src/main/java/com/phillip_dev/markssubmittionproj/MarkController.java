f34df7ee9c3c279a8c8105c2b6f1c45dfb1e80fb
PHILLIP26 <phillip26@discovery.co.za>
2024-01-26 23:36:40 +0200

    access model

--- src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
+++ src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
@@ -1,12 +1,13 @@
 package com.phillip_dev.markssubmittionproj;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MarkController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Somehtmlview";
+    @GetMapping("/marks")
+    public String getMarks(Model model){
+        return "marks";
     }
 }
