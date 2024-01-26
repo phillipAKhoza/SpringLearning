3da3d4437591629fcfbaf5cd0f8e50f95cca0162
PHILLIP26 <phillip26@discovery.co.za>
2024-01-26 23:31:52 +0200

    simple get request and returning html

---
+++ src/main/java/com/phillip_dev/handlegetrequest/MovieController.java
@@ -0,0 +1,14 @@
+package com.phillip_dev.handlegetrequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MovieController {
+    @GetMapping("/")
+    public String getMovies() {
+        return "shows";
+    }
+    
+}
