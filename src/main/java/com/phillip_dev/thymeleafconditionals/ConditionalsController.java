636939b81892d2a709a3bff6e5a66bc8d2fa0902
phillip khoza <phillipantonk@gmail.com>
2024-01-27 14:35:17 +0200

    dealership proj

--- src/main/java/com/phillip_dev/thymeleafconditionals/ConditionalsController.java
+++ src/main/java/com/phillip_dev/thymeleafconditionals/ConditionalsController.java
@@ -3,7 +3,6 @@ package com.phillip_dev.thymeleafconditionals;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -28,7 +27,12 @@ public class ConditionalsController {
 
         return "painting";
     }
-    
+    @GetMapping("/dealership")
+    public String getMethodName(Model model) {
+        model.addAttribute("budget", 10000 );
+        model.addAttribute("make", "Subaru");
+        return "dealership";
+    }
     
 }
 
