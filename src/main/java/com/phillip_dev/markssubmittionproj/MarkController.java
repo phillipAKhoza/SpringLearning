0d123edf18d1aa95003a8c4f5689762c2a458c39
Phillip khoza <phillipantonk@gmai.com>
2024-01-27 18:48:03 +0200

    thymeleaf loop and bug fix

--- src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
+++ src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
@@ -1,15 +1,24 @@
 package com.phillip_dev.markssubmittionproj;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MarkController {
+    List<Mark> studentMarks = Arrays.asList(
+        new Mark("Phillip", "Programming", 90),
+        new Mark("Nkateko", "Maths", 75),
+        new Mark("Mpendulo", "OOP", 95)
+    );
+
     @GetMapping("/marks")
     public String getMarks(Model model){
-        Mark mark = new Mark("Phillip", "Programming", 90);
-        model.addAttribute("mark",mark);
+    
+        model.addAttribute("marks",studentMarks);
         return "marks";
     }
 }
