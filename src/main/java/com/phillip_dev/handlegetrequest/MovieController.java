201c9e57be554de9d7480d64505c1f73ba2ada25
phillip khoza <phillipantonk@gmail.com>
2024-01-27 09:57:03 +0200

    adding shows to the controller

--- src/main/java/com/phillip_dev/handlegetrequest/MovieController.java
+++ src/main/java/com/phillip_dev/handlegetrequest/MovieController.java
@@ -1,13 +1,25 @@
 package com.phillip_dev.handlegetrequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
 public class MovieController {
     @GetMapping("/")
-    public String getMovies() {
+    public String getMovies(Model model) {
+        Show show1 = new Show("Breaking Bad","Ozymandias", 10.0);
+        Show show2 = new Show("Attack on Titan","Hero", 9.9);
+        Show show3 = new Show("Attack on Titan","Perfect Game", 9.9);
+        Show show4 = new Show("Star Wars: The Clone Wars","Victory and Death", 9.9);
+        Show show5 = new Show("Mr. Robot","407 Proxy Authentication Required", 9.9);
+        model.addAttribute("show1", show1);
+        model.addAttribute("show2", show2);
+        model.addAttribute("show3", show3);
+        model.addAttribute("show4", show4);
+        model.addAttribute("show5", show5);
         return "shows";
     }
     
