8b73500642bd5cae2b6045b5f72835c07d819cdc
phillip khoza <phillipantonk@gmail.com>
2024-01-27 12:21:11 +0200

    proj init

---
+++ src/main/java/com/phillip_dev/thymeleafconditionals/ThymeleafconditionalsApplication.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.thymeleafconditionals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafconditionalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafconditionalsApplication.class, args);
+	}
+
+}
