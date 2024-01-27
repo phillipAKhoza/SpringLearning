b14d4ce9d651fc27ff6472dd268226ea65f24761
phillip khoza <phillipantonk@gmail.com>
2024-01-27 12:21:58 +0200

    project init

---
+++ src/test/java/com/phillip_dev/thymeleafconditionals/ThymeleafconditionalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.thymeleafconditionals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafconditionalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
