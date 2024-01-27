3c132a352f32101451b1595ca734b32d72a698f3
phillip khoza <phillipantonk@gmail.com>
2024-01-27 14:14:43 +0200

    switch case view

--- src/main/java/com/phillip_dev/thymeleafconditionals/ConditionalsController.java
+++ src/main/java/com/phillip_dev/thymeleafconditionals/ConditionalsController.java
@@ -3,6 +3,8 @@ package com.phillip_dev.thymeleafconditionals;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -20,6 +22,13 @@ public class ConditionalsController {
         model.addAttribute("speed", 65);
         return "sign";
     }
+    @GetMapping("/art")
+    public String getPaining(Model model) {
+        model.addAttribute("painting", "starry-night");
+
+        return "painting";
+    }
+    
     
 }
 
