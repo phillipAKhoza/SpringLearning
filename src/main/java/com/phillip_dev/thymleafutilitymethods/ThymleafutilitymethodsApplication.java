2de8e401df59d7fe04f45210965d52d20158b199
PHILLIP26 <phillip26@discovery.co.za>
2024-02-05 16:33:06 +0200

    proj init

---
+++ src/main/java/com/phillip_dev/thymleafutilitymethods/ThymleafutilitymethodsApplication.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.thymleafutilitymethods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafutilitymethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafutilitymethodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phillip_dev/thymleafutilitymethods/ThymleafutilitymethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.thymleafutilitymethods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafutilitymethodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
