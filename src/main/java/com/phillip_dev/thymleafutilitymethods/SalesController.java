2b705a5b8315bc45a8ba4ccd84962ab3a9da2ac4
phillip anton khoza <phillipantonk@gmail.com>
2024-02-05 16:52:42 +0200

    update from double to BigDeciaml

--- src/main/java/com/phillip_dev/thymleafutilitymethods/SalesController.java
+++ src/main/java/com/phillip_dev/thymleafutilitymethods/SalesController.java
@@ -1,5 +1,6 @@
 package com.phillip_dev.thymleafutilitymethods;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SalesController {
 
     List<Record> records = Arrays.asList(
-        new Record("Chair", 20.99 , 5.99),
-        new Record("Table", 40.99 , 8.99),
-        new Record("Chair", 100.99 , 105.99),
-        new Record("Chair", 200.99 , 59.99),
-        new Record("Chair", 150.99 , 205.99)
+        new Record("Chair", new BigDecimal(20.99)  ,new BigDecimal(5.99) ),
+        new Record("Table",  new BigDecimal(40.99 ), new BigDecimal(8.99)),
+        new Record("Chair", new BigDecimal(100.99 ) , new BigDecimal(105.99)),
+        new Record("Chair", new BigDecimal(200.99) , new BigDecimal(59.99)),
+        new Record("Chair", new BigDecimal(150.99) , new BigDecimal(205.99))
     );
 
 
