f94e2e1a2b9d686ccc64ba218d2da422055a0ed0
phillip anton khoza <phillipantonk@gmail.com>
2024-02-05 13:23:29 +0200

    proj init

---
+++ src/test/java/com/phillip_dev/thymleafloop/ThymleafloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.thymleafloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
