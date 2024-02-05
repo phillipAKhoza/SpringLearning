b186588c6df90444ba394781e4b7b8aefda7472e
phillip anton khoza <phillipantonk@gmail.com>
2024-02-05 13:24:20 +0200

    proj init

---
+++ src/main/java/com/phillip_dev/thymleafloop/ThymleafloopApplication.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.thymleafloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafloopApplication.class, args);
+	}
+
+}
