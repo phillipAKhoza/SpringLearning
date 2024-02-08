a57ca6a3fc61c2b6a794e77f6e26b5602cb14acc
Phillip khoza <phillipantonk@gmail.com>
2024-02-08 17:45:04 +0200

    add validation anotation to fields

--- src/main/java/com/phillip_dev/markssubmittionproj/Mark.java
+++ src/main/java/com/phillip_dev/markssubmittionproj/Mark.java
@@ -2,8 +2,12 @@ package com.phillip_dev.markssubmittionproj;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class Mark {
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @NotBlank(message = "Subject cannot be blank")
     private String subject;
     private int score;
     private String id;
