d950ccdba2e34a41dd29d77dbeb4b857a83ed7ae
Phillip khoza <phillipantonk@gmail.com>
2024-02-08 17:44:28 +0200

    add form validation to the post method

--- src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
+++ src/main/java/com/phillip_dev/markssubmittionproj/MarkController.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 
 
@@ -36,10 +39,11 @@ public class MarkController {
     // this is a handler called when you submit the form
     // the handler will get the model of the submited form
     @PostMapping("/handleSubmit")
-    public String submitForm(Mark mark) {
+    public String submitForm(@Valid Mark mark, BindingResult result) {
         int index = getMarkIndex(mark.getId());
         // first you need to check if the data being submitted exits or not and we check that via a getMarkIndex method
         // if the data exits we update .set if not we add .add
+        if(result.hasErrors()) return "form";
         if(index == Constants.NOT_FOUND){
           studentMarks.add(mark);
         }else{
