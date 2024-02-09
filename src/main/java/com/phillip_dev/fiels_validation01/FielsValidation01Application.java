de6b41a5fb6532cfb7f23fd9b192b57ffd4201b7
PHILLIP26 <phillip26@discovery.co.za>
2024-02-09 09:18:21 +0200

    proj init

---
+++ src/main/java/com/phillip_dev/fiels_validation01/FielsValidation01Application.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.fiels_validation01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FielsValidation01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FielsValidation01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/phillip_dev/fiels_validation01/FielsValidation01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.fiels_validation01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FielsValidation01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
