e86f993046d15fc829aaccea03ce0b91315cbdf5
Phillip khoza <phillipantonk@gmail.com>
2024-02-10 14:58:21 +0200

    create a repository file and move data into the class

---
+++ src/main/java/com/phillip_dev/markssubmittionproj/repository/MarkRepository.java
@@ -0,0 +1,10 @@
+package com.phillip_dev.markssubmittionproj.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.phillip_dev.markssubmittionproj.Mark;
+
+public class MarkRepository {
+   private List<Mark> studentMarks = new ArrayList<>();
+}
