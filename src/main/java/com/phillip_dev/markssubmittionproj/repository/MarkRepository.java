1d136ccc1d622ab3c749d39cd784c5c45bcdcc51
Phillip khoza <phillipantonk@gmail.com>
2024-02-10 15:07:10 +0200

    add mark method

--- src/main/java/com/phillip_dev/markssubmittionproj/repository/MarkRepository.java
+++ src/main/java/com/phillip_dev/markssubmittionproj/repository/MarkRepository.java
@@ -7,4 +7,12 @@ import com.phillip_dev.markssubmittionproj.Mark;
 
 public class MarkRepository {
    private List<Mark> studentMarks = new ArrayList<>();
+
+   public Mark getMark(int index){
+    return studentMarks.get(index);
+   }
+
+   public void addMark(Mark mark){
+    studentMarks.add(mark);
+   }
 }
