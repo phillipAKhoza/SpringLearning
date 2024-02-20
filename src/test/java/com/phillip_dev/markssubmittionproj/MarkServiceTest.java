b390892c0179fe97507873e3e60a9bda965769f6
phillip khoza <phillipantonk@gmail.com>
2024-02-20 15:26:46 +0200

    add mockitoJunitRunner class

--- src/test/java/com/phillip_dev/markssubmittionproj/MarkServiceTest.java
+++ src/test/java/com/phillip_dev/markssubmittionproj/MarkServiceTest.java
@@ -1,5 +1,9 @@
 package com.phillip_dev.markssubmittionproj;
 
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MarkServiceTest {
     
 }
