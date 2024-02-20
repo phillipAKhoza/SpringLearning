95b40877c8592e86a76cd5619cfaaa725d6d8f62
phillip khoza <phillipantonk@gmail.com>
2024-02-20 15:27:31 +0200

    add mock repo

--- src/test/java/com/phillip_dev/markssubmittionproj/MarkServiceTest.java
+++ src/test/java/com/phillip_dev/markssubmittionproj/MarkServiceTest.java
@@ -1,9 +1,15 @@
 package com.phillip_dev.markssubmittionproj;
 
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import com.phillip_dev.markssubmittionproj.repository.MarkRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MarkServiceTest {
+    
+    @Mock
+    private MarkRepository markRepository;
     
 }
