be1a324f21077230daf5098cf07a143a49e57d20
phillip khoza <phillipantonk@gmail.com>
2024-02-20 15:28:01 +0200

    inject mocks on service

--- src/test/java/com/phillip_dev/markssubmittionproj/MarkServiceTest.java
+++ src/test/java/com/phillip_dev/markssubmittionproj/MarkServiceTest.java
@@ -1,15 +1,19 @@
 package com.phillip_dev.markssubmittionproj;
 
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.phillip_dev.markssubmittionproj.repository.MarkRepository;
+import com.phillip_dev.markssubmittionproj.service.MarkService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MarkServiceTest {
     
     @Mock
     private MarkRepository markRepository;
-    
+
+    @InjectMocks
+    private MarkService markService;
 }
